d64d42bf89a0079ef2b00946f39b22c01cf49b82
Joseph Kim <josek16@uci.edu>
2024-03-01 08:55:28 -0800

    initial model files

---
+++ scrape/models/Round.py
@@ -0,0 +1,9 @@
+class Round:
+    def __init__(self, team_loadout: str, enemy_loadout: str, team_score: int, enemy_score: int, map: str, round_outcome, match_outcome):
+        self.team_loadout = team_loadout
+        self.enemy_loadout = enemy_loadout
+        self.team_score = team_score
+        self.enemy_score = enemy_score
+        self.map = map
+        self.round_outcome = round_outcome
+        self.match_outcome = match_outcome
