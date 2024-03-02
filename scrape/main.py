a1c573f818540b2594ba326f8874be794a7ceeef
Joseph Kim <josek16@uci.edu>
2024-03-02 12:13:37 -0800

    Update main.py

--- scrape/main.py
+++ scrape/main.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     soup = VLRScraper.fetch_page_soup(
         'https://www.vlr.gg/303095/team-heretics-vs-karmine-corp-champions-tour-2024-emea-kickoff-playoffs-gf/?game=all&tab=economy')
 
-    maps = VLRScraper.fetch_match_data(soup)
+    match = VLRScraper.fetch_match_data(soup)
 
     # VLRScraper.save_soup_to_file(maps[1], 'soup.txt')
