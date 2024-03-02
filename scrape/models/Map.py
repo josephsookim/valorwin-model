53912b0922f81fde12dffb236c7101c065207876
Joseph Kim <josek16@uci.edu>
2024-03-02 03:23:17 -0800

    add map outcome to map object

--- scrape/models/Map.py
+++ scrape/models/Map.py
@@ -1,4 +1,5 @@
 class Map:
-    def __init__(self, map_name: str):
+    def __init__(self, map_name: str, map_outcome: int):
         self.map_name = map_name
+        self.map_outcome = map_outcome
         self.rounds = []
