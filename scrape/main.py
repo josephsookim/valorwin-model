b2da6d7e059ecbe30bb0a0ae1230c20cc1c428b5
Joseph Kim <josek16@uci.edu>
2024-03-07 15:46:28 -0800

    Update main.py

--- scrape/main.py
+++ scrape/main.py
@@ -4,29 +4,22 @@ import time
 import os
 
 if __name__ == '__main__':
-    url_list = []
+    url_list = VLRScraper.load_list_from_file('scrape/data/results.txt')
 
-    with open('scrape/data/events.txt', 'r') as file:
-        i = 1
-        for url in file:
-            try:
-                soup = VLRScraper.fetch_page_soup(url.strip())
-                urls = VLRScraper.fetch_page_match_urls(soup)
-                url_list.extend(urls)
-                print(f'done {i}', len(urls))
+    i = 1
+    for url in url_list:
+        try:
+            soup = VLRScraper.fetch_page_soup(url)
+            match = VLRScraper.fetch_match_data(soup)
 
-            except:
-                print(f'error {i}')
+            print(f'done {i}')
 
-            finally:
-                i += 1
-                time.sleep(1)
+        except:
+            print(f'error {i}')
 
-    with open('scrape/data/results.txt', 'w') as file:
-        # Iterate through each URL in the list
-        for url in url_list:
-            # Write each URL to the file, followed by a newline character
-            file.write(url + '\n')
+        finally:
+            i += 1
+            time.sleep(1)
 
     '''
     soup = VLRScraper.fetch_page_soup(
