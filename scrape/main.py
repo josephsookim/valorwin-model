d1cc6faf2fd62d7bc632bdc618beb503340024f3
Joseph Kim <josek16@uci.edu>
2024-03-06 23:48:31 -0800

    test

--- scrape/main.py
+++ scrape/main.py
@@ -1,9 +1,32 @@
 from utils.scraper import VLRScraper
 from utils.database import MongoDB
+import time
+import os
 
 if __name__ == '__main__':
-    soup = VLRScraper.fetch_page_soup(
-        'https://www.vlr.gg/event/matches/1925/champions-tour-2024-emea-kickoff/?series_id=3721')
+    url_list = []
+
+    with open('scrape/data/events.txt', 'r') as file:
+        i = 1
+        for url in file:
+            try:
+                soup = VLRScraper.fetch_page_soup(url.strip())
+                urls = VLRScraper.fetch_page_match_urls(soup)
+                url_list.extend(urls)
+                print(f'done {i}', len(urls))
+
+            except:
+                print(f'error {i}')
+
+            finally:
+                i += 1
+                time.sleep(1)
+
+    with open('scrape/data/results.txt', 'w') as file:
+        # Iterate through each URL in the list
+        for url in url_list:
+            # Write each URL to the file, followed by a newline character
+            file.write(url + '\n')
 
     '''
     soup = VLRScraper.fetch_page_soup(
