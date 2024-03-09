c56d2acb8fbf3664e22240a8a3a5c173a5b8c666
Joseph Kim <josek16@uci.edu>
2024-03-09 10:42:42 -0800

    Update Round.py

--- scrape/models/Round.py
+++ scrape/models/Round.py
@@ -1,5 +1,5 @@
 class Round:
-    def __init__(self, team_loadout: str, enemy_loadout: str, team_score: int, enemy_score: int, round_outcome):
+    def __init__(self, team_loadout: str, enemy_loadout: str, team_score: int, enemy_score: int, round_outcome: int):
         self.team_loadout = team_loadout
         self.enemy_loadout = enemy_loadout
         self.team_score = team_score
